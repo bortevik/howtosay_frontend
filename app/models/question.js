e3abca3209ca4d8020ce8ab032f83a69c2f9e3c3
bortevik <bortevik@gmail.com>
2016-01-23 22:13:17 +0500

    Update ember and ember-data

--- app/models/question.js
+++ app/models/question.js
@@ -1,7 +1,9 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
-const { attr } = DS;
+export default Model.extend({
+  text: attr('string'),
 
-export default DS.Model.extend({
-  text: attr('string')
+  answer: hasMany()
 });
